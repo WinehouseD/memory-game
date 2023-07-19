040649d7a4d3ecb820bf0cac980da8b80e7a1a1a
Diana <kitdiana6@gmail.com>
2023-07-19 20:43:57 +0300

    main changes

---
+++ card.js
@@ -0,0 +1,42 @@
+/**
+ * describes front or back side of each card
+ */
+
+class Card {
+  #image;
+  #element;
+  #isFlipped = false;
+
+  constructor(image) {
+    this.#image = image;
+
+    this.#element = document.querySelector("div");
+    this.#element.classList.add("card");
+    this.#element.style.backgroundImage = `url('${this.coverPath}')`;
+    this.#element.connectedCard = this;
+  }
+
+  get imagePath() {
+    return `images/${this.image}`;
+  }
+
+  get coverPath() {
+    return `images/card-cover.png`;
+  }
+
+  get element() {
+    return this.#element;
+  }
+
+  flip() {
+    if (this.#isFlipped)
+      this.#element.style.backgroundImage = `url('${this.coverPath}')`;
+    else this.#element.style.backgroundImage = `url('${this.imagePath}')`;
+
+    this.#isFlipped = !this.#isFlipped;
+  }
+
+  disconectFromDOM() {
+    this.#element.connectedCard = null;
+  }
+}
---
+++ deck.js
@@ -0,0 +1,40 @@
+/**
+ * cards deck. class for creating and shuffling cards. includes list of cards images
+ */
+
+class Deck {
+  #cardsImages = [
+    "apple.png",
+    "cake.png",
+    "car.png",
+    "cat.png",
+    "cherry.png",
+    "factory.png",
+    "house.png",
+    "plane.png",
+    "ship.png",
+    "train.png",
+  ];
+
+  constructor() {
+    this.cards = [];
+    this.#cardsImages.forEach((image) => {
+      this.cards.push(new Card(image));
+      this.cards.push(new Card(image));
+    });
+  }
+
+  shuffle() {
+    this.cards.sort(() => Math.random() - 0.5);
+  }
+
+  removeCard(card) {
+    let index = this.cards.findIndex(
+      (item) => item.imagePath == card.imagePath
+    );
+    if (index != -1) {
+      this.cards.splice(index, 1);
+      card.disconectFromDOM();
+    }
+  }
+}
